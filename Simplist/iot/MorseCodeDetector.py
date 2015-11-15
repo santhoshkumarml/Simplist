d92f268fadb06275d462d381d1ac36b83d0fc7b3
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-11-14 22:14:41 -0500

    Add python packages

--- Simplist/iot/MorseCodeDetector.py
+++ Simplist/iot/MorseCodeDetector.py
@@ -3,26 +3,26 @@ import json
 LUX = 'lux'
 DEVICE_ID = 'DEVICEID'
 END_OF_COMMUNICATION = ''
-class MorseCodeDetector():
+
+
+class MorseCodeDetector:
     def __init__(self, deviceId):
         self.deviceId = deviceId
-		self.text = ''
-		self.in_progress = false
+        self.in_progress = False
+        self.text = ''
 
     def handle_request(self, json_data):
-		if not self.in_progress:
-			self.in_progress = true
+        if not self.in_progress:
+            self.in_progress = True
         info = json.load(json_data)
-        val = char(info[LUX])
-		if val == END_OF_COMMUNICATION:
-			self.in_progress = false
-		else:
-			self.text = self.text + val
-	
-	def get_in_progress():
-		return self.in_progress
-		
-	def get_text():
-		return self.text
-		
-		
+        val = chr(info[LUX])
+        if val == END_OF_COMMUNICATION:
+            self.in_progress = True
+        else:
+            self.text += val
+
+    def get_in_progress(self):
+        return self.in_progress
+
+    def get_text(self):
+        return self.text
